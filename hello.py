d6e7986d39b8da26ce955d8971037668d9dbc9f1
Данилов Алексей <dmasalex@gmail.com>
2022-06-05 22:37:54 +0300

    add forms

--- hello.py
+++ hello.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'djnhfjk3jh221jjj4h56vj7&89'
+
+
+class NamerForm(FlaskForm):
+	name = StringField("What's Your Name", validators=[DataRequired()])
+	submit = SubmitField("Submit")
 
 
 @app.route('/')
-def hello():
+def index():
 	return render_template("index.html")
 
 
@@ -21,3 +30,22 @@ def page404(e):
 @app.errorhandler(500)
 def page500(e):
 	return render_template("500.html"), 500
+
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+	name = None
+	form = NamerForm()
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ''
+	return render_template('name.html',
+		name = name,
+		form = form)
+
+
+
+
+
+
+
