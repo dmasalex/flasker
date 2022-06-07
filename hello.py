7e5179fb1910541f0e3ca7e99c5a594936635913
Данилов Алексей <dmasalex@gmail.com>
2022-06-07 23:47:22 +0300

    add models

--- hello.py
+++ hello.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SECRET_KEY'] = 'djnhfjk3jh221jjj4h56vj7&89'
+
+db = SQLAlchemy(app)
+
+
+class UserForm(FlaskForm):
+	name = StringField("Name", validators=[DataRequired()])
+	email = StringField("Email", validators=[DataRequired()])
+	submit = SubmitField("Submit")
+
+
+class Users(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(200), nullable=False)
+	email = db.Column(db.String(200), nullable=False, unique=True)
+	date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+	def __repr__(self):
+		return '<Name %r>' % self.name
 
 
 class NamerForm(FlaskForm):
@@ -14,6 +36,7 @@ class NamerForm(FlaskForm):
 
 @app.route('/')
 def index():
+	flash("Welcome to our website!")
 	return render_template("index.html")
 
 
@@ -39,9 +62,36 @@ def name():
 	if form.validate_on_submit():
 		name = form.name.data
 		form.name.data = ''
-	return render_template('name.html',
+		flash("It's ok!")
+	return render_template(
+		'name.html',
 		name = name,
 		form = form)
+
+
+@app.route('/user/add', methods=['GET', 'POST'])
+def add_user():
+	name = None
+	form = UserForm()
+	if form.validate_on_submit():
+		user = Users.query.filter_by(email=form.email.data).first()
+		if user is None:
+			user = Users(
+				name=form.name.data,
+				email=form.email.data)
+			db.session.add(user)
+			db.session.commit()
+		name = form.name.data
+		form.name.data = ''
+		form.email.data = ''
+		flash("User add Successfully!!!")
+	our_users = Users.query.order_by(Users.date_added)
+	return render_template(
+		'add_user.html',
+		form=form,
+		name=name,
+		our_users=our_users
+		)
 
 
 
